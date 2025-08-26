a51568cfa40e019648d9bf6c8dc8964cb7d1c706
Shahadul_Kobir <525unlimited525@gmail.com>
2025-08-26 14:51:54 +0600

    Update area_of_a_90_degree_triangle.py

--- area_of_a_90_degree_triangle.py
+++ area_of_a_90_degree_triangle.py
@@ -1,8 +1,16 @@
-print("welcome to my multiplication only calculator")
-x = float(input("choose your first number:"))
-y = float(input("choose your second number:"))
-result = x * y
-if result.is_integer():
-    print(f"the answer is: {int(result)}")
-else:
-    print(f"the answer is: {result}")
+import math
+
+print("*notice that the answer will always show up in cm^2")
+
+# take the user inputs for 2 sides of the triangle
+
+a = float(input("Ente the value of A:"))
+b = float(input("Ente the value of B:"))
+
+# now we calculate the value of the other side known as 'c' using math library
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+# now we print the value of 'c' and round the value so that it looks good!
+
+print(f"The value of C is:{round(c, 2)}cm^2")
